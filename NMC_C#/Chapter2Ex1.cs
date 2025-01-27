295859c7f4d176a61496396ab912f2df43b76ca5
Braden <bradenleach111@gmail.com>
2025-01-27 13:32:12 -0500

    Update Chapter2Ex1.cs

--- NMC_C#/Chapter2Ex1.cs
+++ NMC_C#/Chapter2Ex1.cs
@@ -1,19 +1,22 @@
 using System;
 namespace PieceRatePay
 {
- class Program
- {
- static void Main(string[] args)
- {
- // prompt the user to enter a whole number for the hours worked
- // create an integer variable named hours and set it equal to the value you read in from the console
- // prompt the user to enter a number for the salary - this number may include decimal places
- // create a double variable named salary and set it equal to the value you read in from the console
- // calculate the workers hourly rate by dividing the salary by the hours worked and store the value
- // in a variable called averageHourlyPay (because you are dividing a double value by an integer, the data
- // type for the averageHourlyPay should be double)
- // using string interpolation to print the following phrase (numbers should be formatted to display 2 decimal places)
- // "You worked {hours} hours and were paid a salary of {salary}, your average hourly pay is {averageHourlyPay}”);
- } 
- }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompt the user to enter a whole number for the hours worked
+            Console.Writeline("Enter a whole number for the hours worked: ");
+            // create an integer variable named hours and set it equal to the value you read in from the console
+            int hours = Console.ReadLine();
+            // prompt the user to enter a number for the salary - this number may include decimal places
+            Console.Writeline("Enter a number for the salary: ");
+            // create a double variable named salary and set it equal to the value you read in from the console
+            // calculate the workers hourly rate by dividing the salary by the hours worked and store the value
+            // in a variable called averageHourlyPay (because you are dividing a double value by an integer, the data
+            // type for the averageHourlyPay should be double)
+            // using string interpolation to print the following phrase (numbers should be formatted to display 2 decimal places)
+            // "You worked {hours} hours and were paid a salary of {salary}, your average hourly pay is {averageHourlyPay}”);
+        } 
+    }
 }
