d7c1c7d43408961d838c828d9e22374053ebe62c
Braden <bradenleach111@gmail.com>
2025-01-28 08:57:00 -0500

    Update Chapter2Ex1.cs

--- NMC_C#/Chapter2Ex1.cs
+++ NMC_C#/Chapter2Ex1.cs
@@ -6,17 +6,20 @@ namespace PieceRatePay
         static void Main(string[] args)
         {
             // prompt the user to enter a whole number for the hours worked
-            Console.Writeline("Enter a whole number for the hours worked: ");
+            Console.WriteLine("Enter a whole number for the hours worked: ");
             // create an integer variable named hours and set it equal to the value you read in from the console
-            int hours = Console.ReadLine();
+            int hours = int.Parse(Console.ReadLine());
             // prompt the user to enter a number for the salary - this number may include decimal places
-            Console.Writeline("Enter a number for the salary: ");
+            Console.WriteLine("Enter a number for the salary: ");
             // create a double variable named salary and set it equal to the value you read in from the console
+            double salary = double.Parse(Console.ReadLine());
             // calculate the workers hourly rate by dividing the salary by the hours worked and store the value
             // in a variable called averageHourlyPay (because you are dividing a double value by an integer, the data
             // type for the averageHourlyPay should be double)
+            double averageHourlyPay = salary / hours;
             // using string interpolation to print the following phrase (numbers should be formatted to display 2 decimal places)
             // "You worked {hours} hours and were paid a salary of {salary}, your average hourly pay is {averageHourlyPay}”);
+            Console.WriteLine($"You worked {hours} hours and where paid a salary of {salary}, your average hourly pay is {averageHourlyPay:F2} ");
         } 
     }
 }
