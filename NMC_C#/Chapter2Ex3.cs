8f3f35830595a001f03d460a031b02a5698784f0
Braden <bradenleach111@gmail.com>
2025-01-28 13:38:36 -0500

    Update Chapter2Ex3.cs

--- NMC_C#/Chapter2Ex3.cs
+++ NMC_C#/Chapter2Ex3.cs
@@ -1,1 +1,31 @@
+namespace myMusic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //---Song they like
+            Console.WriteLine("What is the name of a song you like to listen to? ");
+            string? songName = Console.ReadLine();
 
+            //---Band of song
+            Console.WriteLine("What is the Name of the bank in the song? ");
+            string? bandName = Console.ReadLine();
+
+            //---Prompt user to input Character
+            Console.WriteLine('What is the genre of music: P for pop, R for rock, M for heavy metal, A for alternative, C for country, J for jazz');
+
+            //---Convert to Char
+            char bandChar = Convert.ToChar(Console.Read());
+            Console.ReadLine();
+
+            //---Song Listened Time
+            Console.WriteLine("How many times have you listened to this song in the past week? ");
+            int timesListened = int.Parse(Console.ReadLine());
+
+            //---Displaying output
+            Console.WriteLine($"You have been listening to {songName} by {bandName}. The song genre is {bandChar} and you have listened to the song approximately {timesListened} times this week.");
+
+        }
+    }
+}
