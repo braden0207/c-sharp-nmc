7ebb77dc900031accba58e23a7005f7f254cdf56
Braden <bradenleach111@gmail.com>
2025-01-28 09:14:34 -0500

    Update Chapter2Ex2.cs

--- NMC_C#/Chapter2Ex2.cs
+++ NMC_C#/Chapter2Ex2.cs
@@ -1,28 +1,28 @@
-using System;
 namespace ItsMyParty
 {
- class Program
- {
- static void Main(string[] args)
- {
- // prompt the user to enter their name
- // create a string variable called myName and set it equal to the value you read in from the console
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompt the user to enter their name
+            Console.WriteLine("Please enter your name: ");
+            // create a string variable called myName and set it equal to the value you read in from the console
+            string myName = Console.ReadLine();
+            // prompt the user to enter their birthday
+            // create a string variable named myBirthday and set it equal to the value you read in from the console
 
- // prompt the user to enter their birthday
- // create a string variable named myBirthday and set it equal to the value you read in from the console
+            // prompt the user to enter their favorite kind of cake
+            // create a string variable named cake and set it equal to the value you read in from the console
 
- // prompt the user to enter their favorite kind of cake
- // create a string variable named cake and set it equal to the value you read in from the console
+            // prompt the user to enter their ideal location for a birthday party 
+            // create a string variable named venue and set it equal to the value you read in from the console
 
- // prompt the user to enter their ideal location for a birthday party 
- // create a string variable named venue and set it equal to the value you read in from the console
-
- // prompt the user to enter the number of guests they are inviting to the party
- // create an integer variable named guests and set it equal to the value you read in from the console
-
- // using string interpolation, print the following information
- // "{myName} has a birthday on {myBirthday}. There will be a party at {venue} with {guests} people."
- // "Save room for the {cake} cake!"
- }
- }
+            // prompt the user to enter the number of guests they are inviting to the party
+            // create an integer variable named guests and set it equal to the value you read in from the console
+    
+            // using string interpolation, print the following information
+            // "{myName} has a birthday on {myBirthday}. There will be a party at {venue} with {guests} people."
+            // "Save room for the {cake} cake!"
+        }
+    }
 } 
