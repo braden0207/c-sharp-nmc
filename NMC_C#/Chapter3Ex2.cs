01800843d6338720b85440d09c44dda389bafbe9
Braden <bradenleach111@gmail.com>
2025-02-03 13:57:31 -0500

    Update and rename Chapter3Ex2 to Chapter3Ex2.cs

---
+++ NMC_C#/Chapter3Ex2.cs
@@ -0,0 +1,44 @@
+namespace hoursCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Constant variables
+            const double discountPrice = 5.00;
+            bool lateShift = false;
+            bool earlyShift = false;
+            int hours = 40;
+            double pay = 50.00;
+            double salary = 0.0;
+
+
+            //--Entering data--
+                Console.Write("Do you work mornings? (type y if it is true, otherwise press any key).\n");
+                char? Morning = char.Parse(Console.ReadLine());
+                
+            //--If Statements--
+                //--Early or Late--
+                if (Morning == 'y' || Morning == 'Y')
+                {
+                    earlyShift = true;
+                }
+                else
+                {
+                    lateShift = true;
+                }
+                //--If Early--
+                if (earlyShift)
+                {
+                    salary = hours * pay;
+                }
+                else 
+                {
+                    salary = hours * pay + 500;
+                }
+            Console.WriteLine($"Hours worked: {hours}");
+            Console.WriteLine($"Salary: {salary:C}");
+        }
+    }      
+}
+
