5ec8288763b701d6c370b1f0b62dbe85ef049efc
Braden <bradenleach111@gmail.com>
2025-02-03 22:53:24 -0500

    Update Chapter2Ex3revise.cs

--- NMC_C#/Chapter2Ex3revise.cs
+++ NMC_C#/Chapter2Ex3revise.cs
@@ -17,14 +17,38 @@ namespace myMusic
             //---Convert to Char
             char bandChar = Convert.ToChar(Console.Read());
             Console.ReadLine();
+            //covert switch
+            string fullgenre;
+            switch (char.ToUpper(bandChar))
+            {
+                case 'P':
+                    fullgenre = "pop";
+                    break;
+                case 'p':
+                     fullgenre = "pop";
+                     break;
+
+
+            }
 
             //---Song Listened Time
             Console.WriteLine("How many times have you listened to this song in the past week? ");
-            int timesListened = int.Parse(Console.ReadLine());
+            int timesListened = Convert.ToInt32((Console.ReadLine()));
 
             //---Displaying output
-            Console.WriteLine($"You have been listening to {songName} by {bandName}. The song genre is {bandChar} and you have listened to the song approximately {timesListened} times this week.");
-
+            Console.WriteLine($"You have been listening to {songName} by {bandName}. The song genre is {fullgenre} and you have listened to the song approximately {timesListened} times this week.");
+            //To make this change, create a new string variable called fullGenre. Then create a switch
+            // statement and check the genre variable. Your case statements should check for the
+            // different genre letters and then they should store the full genre name in the new
+            // variable.
+            // Example:
+            // case ‘p’:
+            // case ‘P’:
+            // fullGenre = “pop”;
+            // break;
+            // 3. Modify the WriteLine statement to print the full genre instead of the shortened version.
+            // 4. Once your program runs correctly, submit the assignment as directed by your
+            // instructor.
         }
     }
 }
