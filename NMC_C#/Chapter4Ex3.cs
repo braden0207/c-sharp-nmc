96743e2f823a7860190adf4421e6494e849e0ec2
Braden <bradenleach111@gmail.com>
2025-02-07 14:07:16 -0500

    Update Chapter4Ex3.cs

--- NMC_C#/Chapter4Ex3.cs
+++ NMC_C#/Chapter4Ex3.cs
@@ -1,1 +1,45 @@
+// The program should do the following:
+// • Welcome the customer
+// • Determine how many ice cream cones the customer is going to purchase.
+// • For each ice cream cone the customer purchases, you need to know:
+// o The size of the cone
+// o The flavor (or flavors) of ice cream
+// • You should total the cost of the cones and let the customer know how much to
+// pay. The amount should be formatted with 2 decimal places.
 
+// You are writing code for an ice cream shop. The shop sells 3 different size cones: small,
+// medium and large. Small cones get 1 scoop of ice-cream. Medium cones can have 2
+// scoops with 2 different flavors. Large cones can have 3 scoops with 3 different flavors.
+// The cost of a small cone is 2.50. The cost of a medium cone is 3.00. The cost of a large
+// cone is 3.50
+
+// This program is similar to Programming Example 1 For Loops, Exercise #2. You will need to
+// create a For loop to create all the cones the customer wants. Inside the for loop, you will
+// ask for the size of the cone. You should use a switch statement to check the size and then
+// ask for the flavor or flavors of ice cream AND to add the dollar amount to a total. The For
+// Loop example includes prompting the user for the price and quantity, in addition to adding
+// to a total. It doesn’t include a switch statement (you will need to look at chapter 3 for
+// information on that).
+
+
+//------Sample output------//
+// Welcome to our ice cream shop!
+// How many cones are you purchasing today?
+// 2
+// What size cone do you want?
+// large
+// What is your first flavor?
+// chocolate chip
+// What is your second flavor?
+// chocolate
+// What is your third flavor?
+// cookie dough
+// Cone # 1 is done.
+// What size cone do you want?
+// medium
+// What is your first flavor?
+// cinnamon
+// What is your second flavor?
+// vanilla
+// Cone # 2 is done.
+// Thank you for your purchase, your total is: $6.50
