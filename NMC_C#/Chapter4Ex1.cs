5fe15d988be279bfa057abe61550122f70d67308
Braden <bradenleach111@gmail.com>
2025-02-09 00:09:45 -0500

    Update Chapter4Ex1.cs

--- NMC_C#/Chapter4Ex1.cs
+++ NMC_C#/Chapter4Ex1.cs
@@ -1,39 +1,27 @@
 using System;
-public class Program
-{
- public static void Main()
- {
-//You will be copying a program and converting a for loop into a while loop, and converting
-//another for loop into a do…while loop.
- double price = 0;
- int i;
- for (i = 1; i <= 5; i++)
- {
- Console.WriteLine($"Enter the price of item# {i}");
- price += double.Parse(Console.ReadLine());
- }
- Console.WriteLine($"Your total for {i-1} items is: ${price}");
- price = 0;
- //convert to while loop
- for (i = 1; i <= 5; i++)
- {
- Console.WriteLine($"Enter the price of item# {i}");
- price += double.Parse(Console.ReadLine());
- }
- Console.WriteLine($"Your total for {i-1} items is: ${price}");
- price = 0;
- //convert to do...while loop
- for (i = 1; i <= 5; i++)
- {
- Console.WriteLine($"Enter the price of item# {i}");
- price += double.Parse(Console.ReadLine());
- }
- Console.WriteLine($"Your total for {i-1} items is: ${price}");
- }
-}
-
-//Convert the loop below each comment to a while loop and do… while loop. The
-// procedure for converting for loops to while loops is on page 24. Do…while loops are
-// discussed on page 25 (the conversion process is similar).
-// 4. Once you have modified the program and it runs correctly, follow your instructor’s
-// directions for submitting the assignment.
+     public class Program
+     {
+          public static void Main()
+          {
+               double price = 0;
+               int i = 1;
+               //convert to while loop
+               while(i <= 5)
+               {
+                    Console.WriteLine($"Enter the price of item# {i}");
+                    price += double.Parse(Console.ReadLine());
+                    i++;
+               }
+               Console.WriteLine($"Your total for {i-1} items is: ${price}");
+               //convert to do...while loop
+               price = 0;
+               i = 1;
+               do
+               {
+                    Console.WriteLine($"Enter the price of item# {i}");
+                    price += double.Parse(Console.ReadLine());
+                    i++;
+               } while (i <= 5);
+               Console.WriteLine($"Your total for {i-1} items is: ${price}");
+          }
+     }
