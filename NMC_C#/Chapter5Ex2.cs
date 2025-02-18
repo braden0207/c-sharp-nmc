1ee58673f1977287610443232dfe8daebf9e41b0
Braden <bradenleach111@gmail.com>
2025-02-17 22:49:06 -0500

    Update Chapter5Ex2.cs

--- NMC_C#/Chapter5Ex2.cs
+++ NMC_C#/Chapter5Ex2.cs
@@ -1,30 +1,58 @@
 using System;
+
 namespace pirateGame
 {
- internal class Program
- {
- static void Main(string[] args)
- {
- bool piratesTurn = true;
- bool winner = false;
- Random rand = new Random();
- int call, toss;
- int numFlips = 0;
- int myScore = 0;
- int pirateScore = 0;
- while (numFlips < 25 && !winner)
- {
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool piratesTurn = true, winner = false;
+            Random rand = new Random();
+            int call, toss, numFlips = 0, myScore = 0, pirateScore = 0;
 
- }
- if (pirateScore > myScore)
- // display a message about walking the plank
- else if (pirateScore == myScore)
- // display a message about being thrown in the hoosegow
- else
- // display a message about receiving a treasure map
- }
- static string GetCoinFace(int coinNumber)
- {
- }
- }
+            while (numFlips < 25 && !winner)
+            {
+                toss = rand.Next(1, 3);
+
+                if (piratesTurn)
+                {
+                    call = rand.Next(1, 3);
+                    if (call == toss) pirateScore += 5;
+                    else pirateScore -= 1;
+
+                    piratesTurn = false;
+                    Console.WriteLine($"Pirates guess = {GetCoinFace(call)}. Toss = {GetCoinFace(toss)}");
+                    Console.WriteLine($"Pirates Score = {pirateScore}");
+                }
+                else
+                {
+                    call = rand.Next(1, 3);
+                    if (call == toss) myScore += 4;
+                    else myScore -= 2;
+
+                    piratesTurn = true;
+                    Console.WriteLine($"My guess = {GetCoinFace(call)}. Toss = {GetCoinFace(toss)}");
+                    Console.WriteLine($"My Score = {myScore}");
+                }
+
+                if (pirateScore >= 50 || myScore >= 50)
+                    winner = true;
+
+                numFlips++;
+            }
+
+            Console.WriteLine("Game Over!");
+            if (pirateScore > myScore)
+                Console.WriteLine($"Pirates win! You scored {myScore}, Pirates scored {pirateScore}. Walk the plank!");
+            else if (pirateScore == myScore)
+                Console.WriteLine($"It's a tie! Both scored {myScore}. Off to the hoosegow!");
+            else
+                Console.WriteLine($"You win! You scored {myScore}, Pirates scored {pirateScore}. Here's your treasure map!");
+        }
+
+        static string GetCoinFace(int coinNumber)
+        {
+            return coinNumber == 1 ? "Heads" : "Tails";
+        }
+    }
 }
