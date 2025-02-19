584a39d310de282ea037ab99bef3e1f3f3a9010d
Braden <bradenleach111@gmail.com>
2025-02-18 23:11:00 -0500

    Update Chapter5Ex2.cs

--- NMC_C#/Chapter5Ex2.cs
+++ NMC_C#/Chapter5Ex2.cs
@@ -1,16 +1,21 @@
 using System;
 
-namespace pirateGame
+namespace PirateGame
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            //-- make bool variables --//
-            bool piratesTurn = true, winner = false;
+            //--variables--//
+            bool piratesTurn = true;
+            bool winner = false;
             Random rand = new Random();
-            //-- make int variables --//
-            int call, toss, numFlips = 0, myScore = 0, pirateScore = 0;
+
+            int call;
+            int toss;
+            int numFlips = 0;
+            int myScore = 0;
+            int pirateScore = 0;
 
             while (numFlips < 25 && !winner)
             {
@@ -19,42 +24,67 @@ namespace pirateGame
                 if (piratesTurn)
                 {
                     call = rand.Next(1, 3);
-                    if (call == toss) pirateScore += 5;
-                    else pirateScore -= 1;
-
+                    if (call == toss)
+                    {
+                        pirateScore += 5;
+                    }
+                    else
+                    {
+                        pirateScore -= 1;
+                    }
                     piratesTurn = false;
-                    Console.WriteLine($"Pirates guess = {GetCoinFace(call)}. Toss = {GetCoinFace(toss)}");
-                    Console.WriteLine($"Pirates Score = {pirateScore}");
+                    Console.WriteLine("Pirates guess = " + GetCoinFace(call) + ". Toss = " + GetCoinFace(toss));
+                    Console.WriteLine("Pirates Score = " + pirateScore);
                 }
                 else
                 {
                     call = rand.Next(1, 3);
-                    if (call == toss) myScore += 4;
-                    else myScore -= 2;
-
+                    if (call == toss)
+                    {
+                        myScore += 4;
+                    }
+                    else
+                    {
+                        myScore -= 2;
+                    }
                     piratesTurn = true;
-                    Console.WriteLine($"My guess = {GetCoinFace(call)}. Toss = {GetCoinFace(toss)}");
-                    Console.WriteLine($"My Score = {myScore}");
+                    Console.WriteLine("My guess = " + GetCoinFace(call) + ". Toss = " + GetCoinFace(toss));
+                    Console.WriteLine("My Score = " + myScore);
                 }
 
                 if (pirateScore >= 50 || myScore >= 50)
+                {
                     winner = true;
+                }
 
                 numFlips++;
             }
-
+            //-- when someone wins or loses --//
             Console.WriteLine("Game Over!");
             if (pirateScore > myScore)
-                Console.WriteLine($"Pirates win! You scored {myScore}, Pirates scored {pirateScore}. Walk the plank!");
+            {
+                Console.WriteLine("Pirates win! You scored " + myScore + ", Pirates scored " + pirateScore + ". Walk the plank!");
+            }
             else if (pirateScore == myScore)
-                Console.WriteLine($"It's a tie! Both scored {myScore}. Off to the hoosegow!");
+            {
+                Console.WriteLine("It's a tie! Both scored " + myScore + ". Off to the hoosegow!");
+            }
             else
-                Console.WriteLine($"You win! You scored {myScore}, Pirates scored {pirateScore}. Here's your treasure map!");
+            {
+                Console.WriteLine("You win! You scored " + myScore + ", Pirates scored " + pirateScore + ". Here's your treasure map!");
+            }
         }
-
+        //-- make coin face to put in other code --//
         static string GetCoinFace(int coinNumber)
         {
-            return coinNumber == 1 ? "Heads" : "Tails";
+            if (coinNumber == 1)
+            {
+                return "Heads";
+            }
+            else
+            {
+                return "Tails";
+            }
         }
     }
 }
