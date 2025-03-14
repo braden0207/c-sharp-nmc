280c93f37864ff2a93e0145f69f34d1423aefc20
Braden <bradenleach111@gmail.com>
2025-03-14 08:54:05 -0400

    Update Ch8Exercise1.cs

--- NMC_C#/Ch8Exercise1.cs
+++ NMC_C#/Ch8Exercise1.cs
@@ -1,1 +1,45 @@
-
+namespace Chapter8Ex1
+{
+ internal class Program
+ {
+ static void Main(string[] args)
+ {
+ string club = string.Empty;
+ double cost = 0;
+ int players = 0;
+ club = changeClub(club);
+ changeCost(cost);
+ changePlayers(players);
+ printEvent(club, cost, players);
+ }
+ private static void changePlayers(int players)
+ {
+ Console.ForegroundColor = ConsoleColor.White;
+ Console.WriteLine("How many players are in the tournement?");
+ players = int.Parse(Console.ReadLine());
+ return;
+ }
+ private static void changeCost(double cost)
+ {
+ Console.ForegroundColor = ConsoleColor.White;
+ Console.WriteLine($"What is the cost per player?");
+ cost = double.Parse(Console.ReadLine());
+ return;
+ }
+ private static string changeClub(string club)
+ {
+ Console.ForegroundColor = ConsoleColor.White;
+ Console.WriteLine("What type of club is holding the competition?");
+ club = Console.ReadLine();
+ return club;
+ }
+ private static void printEvent(string club, double cost, int players)
+ {
+ Console.ForegroundColor = ConsoleColor.DarkGreen;
+ Console.WriteLine($"=========================================================");
+ Console.WriteLine($"Welcome to the {club} club");
+ Console.WriteLine($"The cost to join today's competition is {cost:C}");
+ Console.WriteLine($"The number of players is {players}");
+ Console.WriteLine($"=========================================================");
+ }
+ }
