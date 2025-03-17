ff45e8a6975757dd1a06c2751922140a12ef0658
Braden <bradenleach111@gmail.com>
2025-03-17 08:27:38 -0400

    Update Ch8Exercise1.cs

--- NMC_C#/Ch8Exercise1.cs
+++ NMC_C#/Ch8Exercise1.cs
@@ -1,48 +1,50 @@
+
 namespace Chapter8Ex1
 {
- internal class Program
- {
- static void Main(string[] args)
- {
- string club = string.Empty;
- double cost = 0;
- int players = 0;
- club = changeClub(club);
- changeCost(cost);
- changePlayers(players);
- printEvent(club, cost, players);
- }
- private static void changePlayers(int players)
- {
- Console.ForegroundColor = ConsoleColor.White;
- Console.WriteLine("How many players are in the tournement?");
- players = int.Parse(Console.ReadLine());
- return;
- }
- private static void changeCost(double cost)
- {
- Console.ForegroundColor = ConsoleColor.White;
- Console.WriteLine($"What is the cost per player?");
- cost = double.Parse(Console.ReadLine());
- return;
- }
- private static string changeClub(string club)
- {
- Console.ForegroundColor = ConsoleColor.White;
- Console.WriteLine("What type of club is holding the competition?");
- club = Console.ReadLine();
- return club;
- }
- private static void printEvent(string club, double cost, int players)
- {
- Console.ForegroundColor = ConsoleColor.DarkGreen;
- Console.WriteLine($"=========================================================");
- Console.WriteLine($"Welcome to the {club} club");
- Console.WriteLine($"The cost to join today's competition is {cost:C}");
- Console.WriteLine($"The number of players is {players}");
- Console.WriteLine($"=========================================================");
- }
- }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string club = string.Empty;
+            double cost = 0;
+            int players = 0;
+            club = changeClub(club);
+            changeCost(cost);
+            changePlayers(players);
+            printEvent(club, cost, players);
+        }
+        private static void changePlayers(int players)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("How many players are in the tournement?");
+            players = int.Parse(Console.ReadLine());
+            return;
+        }
+        private static void changeCost(double cost)
+        {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"What is the cost per player?");
+                cost = double.Parse(Console.ReadLine());
+                return;
+        }
+        private static string changeClub(string club)
+        {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("What type of club is holding the competition?");
+                club = Console.ReadLine();
+                return club;
+        }
+        private static void printEvent(string club, double cost, int players)
+        {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine($"=========================================================");
+                Console.WriteLine($"Welcome to the {club} club");
+                Console.WriteLine($"The cost to join today's competition is {cost:C}");
+                Console.WriteLine($"The number of players is {players}");
+                Console.WriteLine($"=========================================================");
+        }
+    }
+}
 
  //Fix the changeCost() method by returning the cost as shown in the example above. Adjust the method call as shown
 // below:
@@ -59,3 +61,4 @@ namespace Chapter8Ex1
 // Modify the method call by adding ref or out before the variable in the argument list. Example: changePlayers(ref players);
 
 // 6. Run the program. You should notice that all values are retained and print correctly.
+
