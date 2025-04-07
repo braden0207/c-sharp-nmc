8b2d20caabafe762f5f7cf1075c73f072f29f615
Braden <bradenleach111@gmail.com>
2025-04-07 08:12:14 -0400

    Update Ch9Ex6RiceKrispies.cs

--- NMC_C#/Ch9Ex6RiceKrispies.cs
+++ NMC_C#/Ch9Ex6RiceKrispies.cs
@@ -1,1 +1,36 @@
-
+using System;
+namespace Ch9Ex6RiceKrispies
+{
+ class Breakfast
+ {
+ static void Main(string[] args)
+ {
+ Snap.talk();
+ Crackle.talk();
+ Pop.talk();
+ Console.Write(" Rice Krispies");
+ Console.WriteLine();
+ }
+ }
+ public class Snap
+ {
+ public static void talk()
+ {
+ Console.Write("Snap ");
+ }
+ }
+ public class Crackle
+ {
+ public static void talk()
+ {
+ Console.Write("Crackle ");
+ }
+ }
+ public class Pop
+ {
+ public static void talk()
+ {
+ Console.Write("Pop ");
+ }
+ }
+}
