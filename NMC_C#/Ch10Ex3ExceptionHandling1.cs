52233dd2276abd50dbde1ec35bb9a29b6359a9ca
Braden <bradenleach111@gmail.com>
2025-04-11 09:00:37 -0400

    Update Ch10Ex3ExceptionHandling1.cs

--- NMC_C#/Ch10Ex3ExceptionHandling1.cs
+++ NMC_C#/Ch10Ex3ExceptionHandling1.cs
@@ -1,1 +1,26 @@
+using System;
+namespace MembersOnly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many items would you like to enter?");
+            int size = int.Parse(Console.ReadLine());
+            string[] students = new string[size];
+            double[] scores = new double[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("Enter the students name: ");
+                students[i] = Console.ReadLine();
+                Console.WriteLine("Enter the test scores: ");
+                scores[i] = double.Parse(Console.ReadLine());
+            }
+            for(int i=0; i<size; i++)
+            {
+                Console.WriteLine($"Student: {students[i]} | Math Score: {scores[i]:0.00}%");
+            }
 
+        }
+    }
+}
