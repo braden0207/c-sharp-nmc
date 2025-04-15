e4a7aa2828c8018fdbffbeccdbed8b13218e3fcd
Braden <bradenleach111@gmail.com>
2025-04-15 08:37:35 -0400

    Update Ch10Ex3ExceptionHandling2.cs

--- NMC_C#/Ch10Ex3ExceptionHandling2.cs
+++ NMC_C#/Ch10Ex3ExceptionHandling2.cs
@@ -1,46 +1,77 @@
 using System;
-namespace calcDiscount
+using System.IO;
+
+namespace Ch10Ex3ExceptionHandling2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //---welcome and prompt---
-            Console.Write("Welcome to the frequent shopper discount program!");
-            Console.Write("Please enter your 4 digit PIN: ");
-            double pin = double.Parse(Console.ReadLine());
-
-            double discount = 0;
-
-            //---spent amount---
-            Console.Write("How much did you spend today?: ");
-            double spent = double.Parse(Console.ReadLine());
-
-            //---discount of pin---
-            if (pin > 5000)
+            try
             {
-                Console.WriteLine("You get 20% a discount!");
-                discount = 0.80;
-            }
-            else if (pin > 1000)
-            {
-                Console.WriteLine("You get a 15% discount!");
-                discount = 0.85;
-            }
-            else if (pin > 100)
-            {
-                Console.WriteLine("You get 10% a discount!");
-                discount = 0.90;
-            }
-            else
-            {
-                Console.WriteLine("You get a 5% discount!");
-                discount = 0.95;
-            }
+                // --Welcome and Prompt--  // 
+                Console.WriteLine("Welcome to the frequent shopper discount program!");
+                Console.Write("Please enter your 4-digit PIN: ");
+                double pin = double.Parse(Console.ReadLine()); // May throw FormatException or OverflowException
 
-            double totalcost = (spent * discount);
-            Console.WriteLine($"Your total cost is {totalcost:C}!");
+                // -- Default Value -- //
+                double discount = 0;
+
+                // --Spent Amount -- //
+                Console.Write("How much did you spend today?: ");
+                double spent = double.Parse(Console.ReadLine()); // May throw FormatException
+
+                // -- Validate Spent Amount -- // 
+                if (spent <= 0)
+                {
+                    throw new InvalidDataException("You cannot receive a discount if you haven't spent any money!");
+                }
+
+                // --Discount of Pin-- //
+                if (pin > 5000)
+                {
+                    Console.WriteLine("You get a 20% discount!");
+                    discount = 0.80;
+                }
+                else if (pin > 1000)
+                {
+                    Console.WriteLine("You get a 15% discount!");
+                    discount = 0.85;
+                }
+                else if (pin > 100)
+                {
+                    Console.WriteLine("You get a 10% discount!");
+                    discount = 0.90;
+                }
+                else
+                {
+                    throw new Exception("You entered an invalid PIN."); // For PINs less than 100
+                }
+
+                // -- Calculate Total Cost -- // 
+                double totalCost = spent * discount;
+                Console.WriteLine($"Your total cost is {totalCost:C}!");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Input error: {ex.Message}");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine($"Overflow error: {ex.Message}");
+            }
+            catch (InvalidDataException ex)
+            {
+                Console.WriteLine($"Data error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Have a great day or fun shopping!");
+            }
         }
     }
 }
-
